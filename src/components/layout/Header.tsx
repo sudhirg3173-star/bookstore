ca9a26ac778cbea63b125d0228bd6d5df70226db
sudhir <sudhir.g3173@gmail.com>
2026-08-12 15:08:10 +0530

    code updated

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -187,7 +187,7 @@ export default function Header({ subjects }: { subjects: string[] }) {
                 🇮🇳 INDEPENDENCE DAY TOP ACCENT
             ====================================================== */}
 
-            <div className="relative h-4 w-full overflow-hidden">
+            <div className="relative h-5 w-full overflow-hidden">
 
                 {/* Saffron - White - Green */}
                 <div
