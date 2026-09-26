d8b391500a6411d0e4198346c2f4f32f3a4b5afb
sudhir <sudhir.g3173@gmail.com>
2026-09-26 12:49:12 +0530

    hero banner updated

--- src/components/home/HeroBanner.tsx
+++ src/components/home/HeroBanner.tsx
@@ -8,8 +8,8 @@ import { ChevronRight } from "lucide-react";
 const slides = [
     {
         id: 1,
-        badge: "BESTSELLER",
-        headline: "Bestselling\n Books",
+        badge: "BOOKS",
+        headline: "Explore\n Books",
         subtext: "Discover popular\n titles of the month",
         cta: "Shop Now",
         href: "/shop",
@@ -29,6 +29,18 @@ const slides = [
         accent: "text-emerald-300",
         bookEmoji: "",
         image: "/images/banner/banner1.webp",
+    },
+    {
+        id: 3,
+        badge: "BOOK OF THE MONTH",
+        headline: "Bestselling\n Books",
+        subtext: "Discover title of the month",
+        cta: "Order Now",
+        href: "https://www.kabdwalbook.com/rewired-mckinseys-playbook-on-how-leading-companies-win-with-technology-and-ai-2nd-edition/eric-lamarre_kate-smaje_robert-levin_alex-singla_alexander-sukharevsky/9781394381906",
+        gradient: "from-[#001329] via-[#001329] to-[#001329]",
+        accent: "text-amber-400",
+        bookEmoji: "",
+        image: "/images/banner/banner4.webp",
     }
 ];
 
